3c2ce0c6f5e47a8db450cb30acfd7278374ef410
Luiz Velho <lvelho@impa.br>
2022-08-09 15:53:59 -0300

    remove unused MSLayer

--- src/networks/siren.py
+++ src/networks/siren.py
@@ -45,37 +45,6 @@ class SineLayer(nn.Module):
         return torch.sin(intermediate), intermediate
 
 
-class MSineLayer(nn.Module):
-
-    def __init__(self, in_features, out_features, bias=True,
-                 is_first=False, omega_0=None):
-        super().__init__()
-        self.omega_0 = omega_0
-        self.is_first = is_first
-
-        self.in_features = in_features
-        self.out_features = out_features
-        self.linear = nn.Linear(in_features, out_features, bias=bias)
-
-        self.init_weights()
-
-    def init_weights(self):
-        with torch.no_grad():
-            if self.is_first:
-                self.linear.weight.uniform_(-self.omega_0 / self.in_features,
-                                             self.omega_0 / self.in_features)
-            else:
-                self.linear.weight.uniform_(-np.sqrt(6 / self.in_features),
-                                             np.sqrt(6 / self.in_features))
-
-    def forward(self, input):
-        return torch.sin(self.linear(input))
-
-    def forward_with_intermediate(self, input):
-        # For visualization of activation distributions
-        intermediate = self.omega_0 * self.linear(input)
-        return torch.sin(intermediate), intermediate
-
 
 class Siren(nn.Module):
     """
