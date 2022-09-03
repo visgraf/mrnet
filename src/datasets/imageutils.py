cb55864cf535e418eb88f8bd1a27b0db61174164
danperazzo <drp@cin.ufpe.br>
2022-09-03 17:13:59 -0300

    [DELETED] Code that is not used

--- src/datasets/imageutils.py
+++ src/datasets/imageutils.py
@@ -21,75 +21,6 @@ def opencv2pil(numpy_image):
     
     return im_pil
 
-# In the future change this to use OpenCV
-
-def convsignal2d(signal, kernel, padding_mode='reflect'):
-    # padding is always 'same'
-
-    if padding_mode == 'zeros':
-        filtered = F.conv2d(signal.data.view(1, signal.channels, -1), 
-                                kernel.view(1, signal.channels, -1), 
-                                padding='same').view(-1)
-    else:
-        raise NotImplementedError("Only zeros padding for now")
-        # kernel_size = kernel.shape
-        # dilation = (1,)
-        # reversed_padding_repeated_twice = [0, 0] * len(kernel_size)
-        
-        # for d, k, i in zip(dilation, kernel_size,
-        #                     range(len(kernel_size) - 1, -1, -1)):
-        #     total_padding = d * (k - 1)
-        #     left_pad = total_padding // 2
-        #     reversed_padding_repeated_twice[2 * i] = left_pad
-        #     reversed_padding_repeated_twice[2 * i + 1] = (
-        #         total_padding - left_pad)
-
-        # filtered = F.conv1d(F.pad(signal.data.view(1, signal.channels, -1), 
-        #                 reversed_padding_repeated_twice, mode='reflect'),
-        #             kernel.view(1, signal.channels, -1))
-
-
-def gaussian_kernel(size=5, device=torch.device('cpu'), channels=1):
-    kernel = torch.tensor([[1., 4., 6., 4., 1],
-                           [4., 16., 24., 16., 4.],
-                           [6., 24., 36., 24., 6.],
-                           [4., 16., 24., 16., 4.],
-                           [1., 4., 6., 4., 1.]])
-    kernel /= 256.
-    kernel = kernel.repeat(channels, 1, 1, 1)
-    kernel = kernel.to(device)
-    return kernel
-
-def box_kernel2D(size=5, device=torch.device('cpu'), channels=1):
-    kernel = torch.ones((size, size)) / size**2
-    kernel = kernel.repeat(channels, 1, 1, 1)
-    kernel = kernel.to(device)
-    return kernel
-
-def downsample(x):
-    return x[:, :, ::2, ::2]
-
-def downsample(signal: ImageSignal, factor=2):
-    if signal.sampling_scheme == Sampling.UNIFORM:
-        return ImageSignal(
-            signal.data[::factor, ::factor, :],
-            signal._width // 2 + 1,
-            signal._height // 2 + 1,
-            signal.coordinates[::factor, ::factor, :]
-        )
-
-    raise(InvalidOperation("Can't decimate a non uniformly sampled signal"))
-
-
-def conv_gauss(img, kernel):
-    n = kernel.shape[-1] - 1
-    nlt = n // 2
-    nrb = n - nlt
-    img = torch.nn.functional.pad(img.unsqueeze(0), 
-                                (nlt, nrb, nlt, nrb), mode='reflect')
-    out = torch.nn.functional.conv2d(img, kernel, groups=img.shape[1])
-    return out
-
 def pyrdown2D(signal, decimate=True):
 
     img_pil = signal.image_pil()
@@ -131,11 +62,8 @@ def gaussian_pyramid2D(signal, levels):
 def gaussian_tower2D(signal, levels):
     '''Only works with box filter for now'''
     
-    kernel = gaussian_kernel()
     tower = [signal]
-    base_size = kernel.shape[-1]
     for s in range(levels-1):
-        kernel = box_kernel2D(base_size * 2**s)
         signal = pyrdown2D(signal, decimate=False)
         tower.append(signal)
     return tower
--- src/training/loss.py
+++ src/training/loss.py
@@ -14,11 +14,6 @@ def filter_active_values(model_val, train_dict, d_code):
     gt = train_dict[d_code][mask]
     return pred, gt
 
-def find_highest_order(orders_str):
-    d_values = sorted(orders_str.split('_')[:-1])
-    d_highest = d_values[-1]
-    # remove d
-    return int(d_highest[1:])
     
 
 def hermite_MSE(output_dict, train_dict, orders):
@@ -28,31 +23,11 @@ def hermite_MSE(output_dict, train_dict, orders):
         pred, gt = filter_active_values(model_out, train_dict, 'd0')
         loss_dict['d0'] = F.mse_loss(pred, gt)
 
-    # commented this part because is not working correctly - lvelho  
     if 'd1' in orders:
         model_d1 = gradient(model_out, coords)
         pred, gt = filter_active_values(model_d1.view(1,-1,1), train_dict, 'd1')
         loss_dict['d1'] = F.mse_loss(pred, gt)
-        
 
-    
-    # if 'd2' in orders:
-    #     if model_d1 is None:
-    #         model_d1 = gradient(model_out, coords)
-    #     model_d2 = gradient(model_d1, coords)
-    #     pred, gt = filter_active_values(model_d2, train_dict, 'd2')
-    #     loss_dict['d2'] = F.mse_loss(pred, gt) * 0.00001 
-    
-    # highest_order = find_highest_order(orders)
-    # derivatives = [model_out]
-    # for i in range(highest_order + 1):
-    #     if i > 0:
-    #         derivatives[i] = gradient(derivatives[i-1], coords)
-    #     d_code = f'd{i}'
-    #     if d_code in orders:
-    #         pred, gt = filter_active_values(model_out, train_dict, d_code)
-    #         loss_dict[d_code] = F.mse_loss(pred, gt)
-    
     return loss_dict
 
 def get_loss_from_map(lossname:str):
