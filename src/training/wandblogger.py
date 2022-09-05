0d932650c581afd552fdb47a300d123b775623c3
Luiz Velho <lvelho@impa.br>
2022-09-05 16:54:27 -0300

    lvelho wandblogger siren inference

--- src/training/wandblogger.py
+++ src/training/wandblogger.py
@@ -43,10 +43,15 @@ class WandBLogger(Logger):
         wandb.log(log_dict)
 
     def on_train_finish(self, trained_model, total_epochs):
+        save_format = self.hyper.get('save_format', None)
+        if save_format is not None:
+            self.log_model(trained_model, save_format)
+        
         wandb.finish()
         print(f'Training finished after {total_epochs} epochs')
 
-#
+##
+
 class WandBLogger2D(WandBLogger):
     
     def on_stage_start(self, current_model, stage_number, updated_hyper=None):
@@ -55,7 +60,7 @@ class WandBLogger2D(WandBLogger):
                 self.hyper[key] = updated_hyper[key]
         
         hyper = self.hyper
-        self.runname = f"{self.name}_{hyper['stage']}/{hyper['max_stages']}_w{hyper['omega_0']}{'T' if hyper['superposition_w0'] else 'F'}_hf{hyper['hidden_features']}_MEp{hyper['max_epochs_per_stage']}_hl{hyper['hidden_layers']}_{hyper['loss_function'][:-3]}{hyper['width']}px"
+        self.runname = f"{self.name}_{hyper['stage']}/{hyper['max_stages']}_w{hyper['omega_0']}{'T' if hyper['superposition_w0'] else 'F'}_hf{hyper['hidden_features']}_MEp{hyper['max_epochs_per_stage']}_hl{hyper['hidden_layers']}_{hyper['width']}px"
         wandb.init(project=self.project, 
                     entity=self.entity, 
                     name=self.runname, 
@@ -72,25 +77,20 @@ class WandBLogger2D(WandBLogger):
         current_model.to(device)
 
         self.log_traindata(train_loader)
-        gt = self.log_groundtruth(test_loader)
-        
+        gt = self.log_groundtruth(test_loader)        
         pred = self.log_prediction(current_model, test_loader, device)
-
         self.log_PSNR(gt.to(device), pred)
 
         extrapolation_interval = self.hyper.get('extrapolate', None)
         if extrapolation_interval is not None:
-            self.log_extrapolation(current_model, 
-                                    extrapolation_interval, 
-                                    test_loader.dataset.dimensions(),
-                                    device)
-
-        save_format = self.hyper.get('save_format', None)
-        if save_format is not None:
-            self.log_model(current_model, save_format)
+            self.log_extrapolation(current_model, extrapolation_interval, 
+                                    test_loader.dataset.dimensions(), device)
 
         current_model.train()
         current_model.to(self.hyper['device'])
+       
+
+##
 
     def log_traindata(self, train_loader):
         traindata = train_loader.dataset.data.view(-1, self.hyper['channels'])
@@ -104,51 +104,33 @@ class WandBLogger2D(WandBLogger):
         self.log_fft(pixels, 'FFT Ground Truth')
         if self.hyper.get('useattributes', False):
             gt_grads = test_loader.dataset.d1
-            self.log_gradmagnitude(gt_grads, 'ground truth')
-        
+            self.log_gradmagnitude(gt_grads, 'Ground Truth')
         return gtdata
 
     def log_prediction(self, model, test_loader, device):
-        model_out = pred_grads = None
-        compute_grads = self.hyper.get('useattributes', False)
-        for X, _ in test_loader:
-            # (1) mudar todas as inferencias com o modelo para esquema abaixo
-            # study to include as a functio of the Model 
-            output_dict = model(test_loader.dataset.coordinates.to(device))
-            batch_out = torch.clamp(output_dict['model_out'], 0.0, 1.0)
-            batch_grads = gradient(batch_out, output_dict['model_in']).detach()
-            
-            model_out = (batch_out.detach() if model_out is None 
-                            else torch.concat([model_out, batch_out.detach()]))
-            if compute_grads:
-                pred_grads = (batch_grads if pred_grads is None
-                            else torch.concat([pred_grads, batch_grads]))
+        output_dict = model(test_loader.dataset.coordinates.to(device))
+        model_out = torch.clamp(output_dict['model_out'], 0.0, 1.0)
 
         pred_pixels = self.as_imagetensor(model_out)
-        # this test probably can be eliminated
-        if pred_pixels.shape[1] <= 1:
-            side = np.sqrt(pred_pixels.shape[2]).astype(int)
-            pred_pixels = torch.reshape(pred_pixels, (side, side)) 
         self.log_imagetensor(pred_pixels, 'Prediction')
         self.log_fft(pred_pixels, 'FFT Prediction')
 
-        if pred_grads is not None:
-            pred_grads = torch.reshape(pred_grads, (-1, 2))
-            self.log_gradmagnitude(pred_grads, 'prediction')
+        if self.hyper.get('useattributes', False):
+            model_grads = gradient(model_out, output_dict['model_in'])
+            pred_grads = torch.reshape(model_grads, (-1, 2))
+            self.log_gradmagnitude(pred_grads, 'Prediction')
+        
         return model_out
     
     # TODO: make it work with color images and non-squared images
 
     def as_imagetensor(self, tensor):
-        w = int(np.sqrt(len(tensor)))
-        h = w
+        w = h = int(np.sqrt(len(tensor)))
         pixels = tensor.cpu().detach().unflatten(0, (w, h))
         return pixels
 
     def log_imagetensor(self, pixels:torch.Tensor, label:str):
-        #image = wandb.Image(pixels.numpy())
-        pmax = torch.max(pixels)
-        pixels = pixels * 255. / pmax 
+        # pixels = pixels / torch.max(pixels) 
         image = wandb.Image(pixels.numpy())    
         wandb.log({label: image})
 
@@ -158,7 +140,7 @@ class WandBLogger2D(WandBLogger):
                         grads[:, :, 1].squeeze(-1).numpy())
         gmin, gmax = np.min(mag), np.max(mag)
         
-        img = Image.fromarray(255 * mag / gmax).convert('L')
+        img = Image.fromarray(255 * (mag - gmin) / gmax).convert('L')
         wandb.log({f'Gradient Magnitude - {label}': wandb.Image(img)})
 
     def log_fft(self, pixels:torch.Tensor, label:str):
@@ -214,6 +196,7 @@ class WandBLogger2D(WandBLogger):
             warnings.warn(
                 f'Model not saved! Save type {save_format} NOT supported.')
             return None
+        print("File ", filename)
         artifact = wandb.Artifact(filename, type='model')
         artifact.add_file(path)
         wandb.log_artifact(artifact)
