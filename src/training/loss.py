0ced3671a165c42a295896b9d8cacad98ee3ee9e
danperazzo <drp@cin.ufpe.br>
2022-09-29 19:48:14 -0300

    [REFACTORED] Loss functions for reconstruction

--- src/training/loss.py
+++ src/training/loss.py
@@ -8,26 +8,35 @@ def gradient(y, x, grad_outputs=None):
     grad = torch.autograd.grad(y, [x], grad_outputs=grad_outputs, create_graph=True)[0]
     return grad
 
-def filter_active_values(model_val, train_dict, d_code):
-    pred = model_val
-    gt = train_dict[d_code]
-    return pred, gt
+def mse_loss(output_dict, train_dict):
+    pred = output_dict['model_out']
+    gt = train_dict['d0']
 
-def hermite_MSE(output_dict, train_dict, orders):
-    model_out, coords = output_dict['model_out'], output_dict['model_in']
     loss_dict = {}
-    if 'd0' in orders:
-        pred, gt = filter_active_values(model_out, train_dict, 'd0')
-        loss_dict['d0'] = F.mse_loss(pred, gt)
+    loss_dict['d0'] = F.mse_loss(pred, gt)
 
-    if 'd1' in orders:
-        model_d1 = gradient(model_out, coords)
-        pred, gt = filter_active_values(model_d1.view(1,-1,1), train_dict, 'd1')
-        loss_dict['d1'] = F.mse_loss(pred, gt)
+    return loss_dict
+
+
+
+def hermite_loss(output_dict, train_dict):
+    coords = output_dict['model_in']
+    
+    pred = output_dict['model_out']
+    gt_grad = train_dict['d1']
+    
+    pred_grad = gradient(pred, coords)
+    pred_grad_flat = pred_grad.view(1,-1,1)
+
+    loss_dict = {}
+    loss_dict['d1'] = F.mse_loss(pred_grad_flat, gt_grad)
 
     return loss_dict
 
 def get_loss_from_map(lossname:str):
-    def loss_func(output_dict, train_dict):
-        return hermite_MSE(output_dict, train_dict, lossname)
-    return loss_func
+
+    if lossname=='mse':
+        return mse_loss
+
+    if lossname=='hermite':
+        return hermite_loss
