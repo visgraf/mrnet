859b524792d9aa30b9065d04b6382042c358c17f
danperazzo <drp@cin.ufpe.br>
2022-11-17 11:34:31 -0300

    [ADDED] Separate file for disc sampler

--- src/datasets/sampler.py
+++ src/datasets/sampler.py
@@ -3,6 +3,7 @@ import scipy
 import numpy as np
 from torch.utils.data import SequentialSampler, BatchSampler
 import torchvision.transforms as T
+from .poisson_disc import PoissonDisc
 
 from .constants import Sampling
 
@@ -84,7 +85,7 @@ class RegularSampler(Sampler):
 class PoissonDiscSampler(RegularSampler):
     def __init__(self, img_data, attributes = [], 
                                                 k = 30, 
-                                                r = 0.8):   
+                                                r = 0.5):   
         transform_to_pil = T.ToPILImage()
         self.img_orig = transform_to_pil(img_data)
 
@@ -94,27 +95,15 @@ class PoissonDiscSampler(RegularSampler):
         self.k = k
         self.r = r
 
-        # Cell side length
-        self.a = self.r/np.sqrt(2)
-
-
     def make_samples(self, data, width, height, batch_pixel_perc):
 
         self.img_data = torch.flatten(data)
         self.img_width = width
         self.img_height = height
 
-        # Number of cells in the x- and y-directions of the grid
-        self.nx, self.ny = int(self.img_width / self.a) + 1, int(self.img_height / self.a) + 1
+        self.sampler_poison = PoissonDisc(width, height, self.r, self.k)
+        self.coords = self.sampler_poison.sample()
 
-        # A list of coordinates in the grid of cells
-        self.coords_list = [(ix, iy) for ix in range(self.nx) for iy in range(self.ny)]
-        # Initilalize the dictionary of cells: each key is a cell's coordinates, the
-        # corresponding value is the index of that cell's point's coordinates in the
-        # samples list (or None if the cell is empty).
-        self.cells = {coords: None for coords in self.coords_list} 
-
-        self.coords = self.create_sample_points_poisson()
         self.size = len(self.coords)
         print(f'Num of samples: {self.size}')
 
@@ -140,120 +129,6 @@ class PoissonDiscSampler(RegularSampler):
         samples = {self.key_group:(in_dict, out_dict)}
 
         return samples 
-
-    def get_cell_coords(self, pt):
-        """Get the coordinates of the cell that pt = (x,y) falls in."""
-
-        return int(pt[0] // self.a), int(pt[1] //  self.a)
-
-    def get_neighbours(self, coords):
-        """Return the indexes of points in cells neighbouring cell at coords.
-
-        For the cell at coords = (x,y), return the indexes of points in the cells
-        with neighbouring coordinates illustrated below: ie those cells that could 
-        contain points closer than r.
-
-                                        ooo
-                                        ooooo
-                                        ooXoo
-                                        ooooo
-                                        ooo
-
-        """
-
-        dxdy = [(-1,-2),(0,-2),(1,-2),(-2,-1),(-1,-1),(0,-1),(1,-1),(2,-1),
-                (-2,0),(-1,0),(1,0),(2,0),(-2,1),(-1,1),(0,1),(1,1),(2,1),
-                (-1,2),(0,2),(1,2),(0,0)]
-        neighbours = []
-        for dx, dy in dxdy:
-            neighbour_coords = coords[0] + dx, coords[1] + dy
-            if not (0 <= neighbour_coords[0] <  self.nx and
-                    0 <= neighbour_coords[1] <  self.ny):
-                # We're off the grid: no neighbours here.
-                continue
-            neighbour_cell =  self.cells[neighbour_coords]
-            if neighbour_cell is not None:
-                # This cell is occupied: store this index of the contained point.
-                neighbours.append(neighbour_cell)
-        return neighbours
-
-    def point_valid(self, pt):
-        """Is pt a valid point to emit as a sample?
-
-        It must be no closer than r from any other point: check the cells in its
-        immediate neighbourhood.
-
-        """
-
-        cell_coords = self.get_cell_coords(pt)
-        for idx in self.get_neighbours(cell_coords):
-            nearby_pt = self.samples[idx]
-            # Squared distance between or candidate point, pt, and this nearby_pt.
-            distance2 = (nearby_pt[0]-pt[0])**2 + (nearby_pt[1]-pt[1])**2
-            if distance2 < self.r**2:
-                # The points are too close, so pt is not a candidate.
-                return False
-        # All points tested: if we're here, pt is valid
-        return True
-
-    def get_point(self, k, refpt):
-        """Try to find a candidate point relative to refpt to emit in the sample.
-
-        We draw up to k points from the annulus of inner radius r, outer radius 2r
-        around the reference point, refpt. If none of them are suitable (because
-        they're too close to existing points in the sample), return False.
-        Otherwise, return the pt.
-
-        """
-        i = 0
-        while i < k:
-            i += 1
-            rho = np.sqrt(np.random.uniform(self.r**2, 4 * self.r**2))
-            theta = np.random.uniform(0, 2*np.pi)
-            pt = refpt[0] + rho*np.cos(theta), refpt[1] + rho*np.sin(theta)
-            if not (0 <= pt[0] < self.img_width and 0 <= pt[1] < self.img_height):
-                # This point falls outside the domain, so try again.
-                continue
-            if self.point_valid(pt):
-                return pt
-
-        # We failed to find a suitable point in the vicinity of refpt.
-        return False
-
-    def create_sample_points_poisson(self):
-        # Pick a random point to start with.
-        pt = (np.random.uniform(0, self.img_width), np.random.uniform(0, self.img_height))
-        self.samples = [pt]
-        # Our first sample is indexed at 0 in the samples list...
-        self.cells[self.get_cell_coords(pt)] = 0
-        # ... and it is active, in the sense that we're going to look for more points
-        # in its neighbourhood.
-        active = [0]
-
-        nsamples = 1
-        # As long as there are points in the active list, keep trying to find samples.
-        while active:
-            # choose a random "reference" point from the active list.
-            idx = np.random.choice(active)
-            refpt = self.samples[idx]
-            # Try to pick a new point rela  tive to the reference point.
-            pt = self.get_point(self.k, refpt)
-            if pt:
-                # Point pt is valid: add it to the samples list and mark it as active
-                self.samples.append(pt)
-                nsamples += 1
-                active.append(len(self.samples)-1)
-                self.cells[self.get_cell_coords(pt)] = len(self.samples) - 1
-            else:
-                # We had to give up looking for valid points near refpt, so remove it
-                # from the list of "active" points.
-                active.remove(idx)
-
-        tensor_samples = torch.tensor(self.samples).float()
-        tensor_samples[:,0] = 1 - 2*tensor_samples[:,0]/self.img_width
-        tensor_samples[:,1] = 1 - 2*tensor_samples[:,1]/self.img_height
-
-        return tensor_samples 
 
 
 class StochasticSampler:
