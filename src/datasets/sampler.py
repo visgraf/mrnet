e343c4a2cd69d07cec9a87778506ddec4fece05b
Hallison Paz <hallison@pgdinamica.com>
2023-04-21 23:22:18 -0300

    refactored sampler to work with multidimensional signals; added Signal1D class

--- src/datasets/sampler.py
+++ src/datasets/sampler.py
@@ -2,7 +2,7 @@ from typing import Sequence, Union
 import torch
 import scipy
 import numpy as np
-from torch.utils.data import SequentialSampler, BatchSampler
+from torch.utils.data import BatchSampler
 import torchvision.transforms as T
 from .poisson_disc import PoissonDisc
 
@@ -27,78 +27,62 @@ def make_grid_coords(nsamples, start, end, dim):
     return grid.reshape(-1, dim)
 
 class Sampler:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data, shape, domain, attributes, batch_size):
+        self.data = data
+        self.data_shape = shape
+        self.domain = domain
+        self.attributes = attributes
+        self.batch_size = batch_size
+        self.batches = []
+        self.make_samples()
 
-    def make_samples(self, image):
-        pass
-
-    def total_size(self):
-        pass
-
-    def get_samples(self, idx):
-        pass
+    def make_samples(self):
+        raise NotImplementedError()
+    
+    def __len__(self):
+        return len(self.batches)
+    
+    def __getitem__(self, idx):
+        return self.batches[idx]
+    
+    def total_nsamples(self):
+        raise NotImplementedError()
+    
 
 class RegularSampler(Sampler):
 
-    def __init__(self, img_data, attributes=[]):   
-        self.img_data = img_data
-        self.attributes = attributes
+    def __init__(self, data, shape, domain, attributes, batch_size):
+        super().__init__(data, shape, domain, attributes, batch_size)
         self.key_group = 'c0'
 
-    def get_tuple_dicts(self, sel_idxs):
-        coords_sel = self.coords[sel_idxs]
-        img_data_sel = self.img_data[sel_idxs]
-            
-        in_dict = {'coords': coords_sel, 'idx':sel_idxs}
-        out_dict = {'d0': img_data_sel.view(-1,1)}
+    def make_samples(self, domain_mask=None):
+        self.coords = make_grid_coords(self.shape, 
+                                       *self.domain, dim=len(self.shape))
+        
+        if domain_mask is None:
+            sampled_indices = torch.randperm(len(self.coords))
+        else:
+            # TODO: permute; flatten?
+            sampled_indices = torch.tensor(range(len(self.coords)))[domain_mask]
 
+        index_batches = list(
+            BatchSampler(sampled_indices, self.batch_size, drop_last=False)
+        )
+        flatdata = torch.flatten(self.data)
+        self.batches = [self.get_tuple_dicts(idx_batch, flatdata) 
+                        for idx_batch in index_batches]
+
+
+    def get_tuple_dicts(self, sel_idxs, flatdata):
+        channels = 1 if len(self.data) == self.data_shape else len(self.data[0])
+        coords_sel = self.coords[sel_idxs]
+        img_data_sel = self.flatdata[sel_idxs]
+        
+        in_dict = {'coords': coords_sel, 'idx':sel_idxs}
+        out_dict = {'d0': img_data_sel.view(-1, channels)}
         samples = {self.key_group:(in_dict, out_dict)}
 
         return samples
-
-    def create_batch_samples(self, batch_pixel_perc, indices_to_sample):
-        batch_size = int(len(indices_to_sample)*batch_pixel_perc)
-
-        return (list(BatchSampler(indices_to_sample, batch_size=batch_size, drop_last=False)))
-
-    def total_size(self):
-        return len(self.list_batches)
-
-    
-    def create_batches(self, batch_index_samples):
-        list_samples = []
-
-        for sel_idxs in batch_index_samples:
-            samples = self.get_tuple_dicts(sel_idxs)
-            list_samples.append(samples)
-        
-        return list_samples
-
-    def make_samples(self, data, width, height, domain, batch_pixel_perc, domain_mask=None):
-        self.img_data = torch.flatten(data)
-        self.img_width = width
-        self.img_height = height
-        self.coords = make_grid_coords((width, height), *domain, dim=2)
-        
-        self.coords_vis = self.coords
-
-        self.batch_index_dict = {}
-        
-        if domain_mask is None:
-            self.size = len(self.coords)
-            self.total_idx_sample = torch.randperm(self.size)
-        else:
-            # TODO: permute
-            self.total_idx_sample = torch.tensor(range(len(self.coords)))[domain_mask]
-            self.size = len(self.total_idx_sample)
-        
-        batch_index = self.create_batch_samples(batch_pixel_perc, self.total_idx_sample)
-
-        self.list_batches = self.create_batches(batch_index)
-
-    def get_samples(self, idx):
-        return self.list_batches[idx]
 
 
 class PoissonDiscSampler(RegularSampler):
@@ -269,15 +253,17 @@ class StratifiedSampler:
 
     
     
-def samplerFactory(sampling_type:Sampling, data_to_sample, attributes):
-    if sampling_type==Sampling.REGULAR:
-        return RegularSampler(data_to_sample, attributes)
+class SamplerFactory:
+    def init(sampling_type:Sampling, data, shape, domain, 
+             attributes, batch_size):
+        if sampling_type==Sampling.REGULAR:
+            return RegularSampler(data, shape, domain, attributes, batch_size)
 
-    elif sampling_type==Sampling.STRATIFIED:
-        return StratifiedSampler(data_to_sample, attributes)
+        elif sampling_type==Sampling.STRATIFIED:
+            return StratifiedSampler(data, attributes)
 
-    elif sampling_type==Sampling.POISSON_DISC:
-        return PoissonDiscSampler(data_to_sample, attributes)
-    
+        elif sampling_type==Sampling.POISSON_DISC:
+            return PoissonDiscSampler(data, attributes)
+        
 
 
