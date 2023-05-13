fixed bug on PSNR computation; changd 3D logger to work with procedural signals (missing PSNR and SSIM)

src/datasets/signals.py
@@ -6,8 +6,11 @@ from PIL import Image
 from torchvision.transforms.functional import to_tensor, to_pil_image
 from pathlib import Path
 from .constants import SAMPLING_DICT, Sampling
-from datasets.sampler import SamplerFactory
+from datasets.sampler import SamplerFactory, make_grid_coords
 from scipy.ndimage import sobel
+from torch.utils.data import BatchSampler
+from scipy.interpolate import RegularGridInterpolator, interpn
+from .utils import rotation_matrix
 
 
 def make_mask(srcpath, mask_color):
@@ -17,6 +20,7 @@ def make_mask(srcpath, mask_color):
     path = path.parent.absolute().joinpath("mask.png")
     Image.fromarray(mask).save(path)
     return str(path)
+
 
 class BaseSignal(Dataset):
     def __init__(self, data,
@@ -54,6 +58,10 @@ class BaseSignal(Dataset):
 
     def size(self):
         return self.data.shape
+    
+    @property
+    def shape(self):
+        return self.size()
     
     def __len__(self):
         return len(self.sampler)
@@ -214,3 +222,154 @@ class VolumeSignal(BaseSignal):
 
     def type_code(self):
         return "3D"
+    
+    def get_slices(self, dircodes):
+        slices = []
+        volume = self.data
+        x_slice = y_slice = z_slice = 0
+        for code in dircodes:
+            if code == 'x':
+                slices.append(volume[:, x_slice, :, :].permute((1, 2, 0)))
+            if code == 'y':
+                slices.append(volume[:, :, y_slice, :].permute((1, 2, 0)))
+            if code == 'z':
+                slices.append(volume[:, :, :, z_slice].permute((1, 2, 0)))
+            if len(code) == 2:
+                code_map = { 'x': [2, 0, 1], 'y': [0, 2, 1], 'z': [0, 1, 2] }
+                dims = self.size()[0]
+                res = self.size()[1]
+                coords = make_grid_coords(res, *self.domain, dims - 1)
+                newdim = 0.0 * torch.ones((len(coords), 1))
+                domain = torch.cat([coords, newdim], dim=-1)
+                domain = domain[:, code_map[code[0]]]
+                rotation = rotation_matrix(code[1], np.pi/4)
+                domain = torch.matmul(rotation, 
+                                        domain.permute((1, 0))).permute((1, 0))
+                
+                # interpolate values
+                points = tuple(np.linspace(*self.domain, res) 
+                                for i in range(dims))
+
+                values = []
+                # from IPython import embed
+                # embed()
+                for c in range(len(self.data)):
+                    values.append(
+                        interpn(points, self.data[c].numpy(), domain.numpy())
+                    )
+                values = torch.from_numpy(np.array(values)).permute((1, 0))
+                slices.append(
+                    values.reshape((res, res, len(self.data))).float())
+        
+        return slices
+
+    
+# TODO make a procedural sampler
+class Procedural3DSignal(Dataset):
+    def __init__(self, procedure,
+                 dims,
+                 channels,
+                 domain=[-1, 1],
+                 attributes=[], 
+                 sampling_scheme=Sampling.REGULAR,
+                 batch_size=0,
+                 YCbCr=False,
+                 shuffle=True):
+        
+        self.procedure = procedure
+        self.dims = dims
+        self.channels = channels
+        # TODO use shuffle
+        self.domain = domain
+        self.batch_size = batch_size
+        self.attributes = attributes
+        self.ycbcr = YCbCr
+        self.data_attributes = {}
+        # if 'd1' in self.attributes:
+        #     self.compute_derivatives()
+        # if isinstance(sampling_scheme, str):
+        #     sampling_scheme = SAMPLING_DICT[sampling_scheme]
+        # self.sampler = SamplerFactory.init(sampling_scheme,
+        #                                    self.data,
+        #                                    self.domain,
+        #                                    self.data_attributes,
+        #                                    batch_size,
+        #                                    shuffle)
+        self.order = 0
+        slice_size = dims[0] * dims[1]
+        self.batches_per_slice = (slice_size // batch_size 
+                                  + 1 if (slice_size % batch_size != 0) else 0)
+
+    def size(self):
+        return (self.channels, *self.dims)
+    
+    @property
+    def shape(self):
+        return self.size()
+    
+    def __len__(self):
+        # return len(self.sampler)
+        return self.batches_per_slice * self.dims[self.order]
+
+    def __getitem__(self, idx):
+        # return self.sampler[idx]
+        try:
+            coords = next(self.coords_sampler)
+        except: #which?
+            self.newdim = torch.linspace(self.domain[0], 
+                                        self.domain[1], 
+                                        self.dims[self.order])
+            # TODO: deal with different sizes in each direction and ORDER
+            coords = make_grid_coords(self.dims[0], 
+                                      *self.domain, 
+                                      len(self.dims)-1)
+            value = self.newdim[idx // self.batches_per_slice]
+            coords = torch.cat([coords, 
+                                value * torch.ones((len(self.newdim)**2, 1))],
+                                dim=-1)
+            self.coords_sampler = iter(BatchSampler(coords, 
+                                               self.batch_size, 
+                                               drop_last=False))
+            coords = torch.stack(next(self.coords_sampler))
+        
+        in_dict = {'coords': coords}
+        out_dict = {'d0': self.procedure(coords)}
+        # if 'd1' in self.attributes.keys():
+        # #     # lazy or not?
+        #     out_dict['d1'] = self.d1[sel_idxs]
+        return {'c0': (in_dict, out_dict)}
+    
+    def type_code(self):
+        return 'P'
+    
+    def new_like(other, data, shuffle=None):
+        raise NotImplementedError("Procedural Signal does not support this operation")
+    
+    def get_slices(self, dircodes):
+        valid_codes = ['x', 'y', 'z', 'xy', 'xz', 'yz']
+        code_map = {
+            'x': [2, 0, 1],
+            'y': [0, 2, 1],
+            'z': [0, 1, 2]
+        }
+        coords = make_grid_coords(self.dims[0], 
+                                  *self.domain, 
+                                  len(self.dims) - 1)
+        newdim = 0.0 * torch.ones((len(coords), 1))
+        domain_slice = torch.cat([coords, newdim], dim=-1)
+        slices = []
+        res = self.dims[0]
+        for code in dircodes:
+            if code not in valid_codes:
+                raise ValueError(
+                    "Direction codes should be in [x, y, z, xy, xz, yz]")
+            domain = domain_slice[:, code_map[code[0]]]
+            if len(code) == 2:
+                # todo, check hot to do it
+                rotation = rotation_matrix(code[1], np.pi/4)
+                domain = torch.matmul(rotation, 
+                                      domain.permute((1, 0))).permute((1, 0))
+            slices.append(
+                self.procedure(domain).reshape(res, res, self.channels))
+
+        return slices

src/datasets/utils.py
@@ -1,6 +1,46 @@
 import torch 
 import numpy as np
+from typing import Sequence, Union
+
 # Adapted from https://github.com/makeyourownalgorithmicart/makeyourownalgorithmicart/blob/master/blog/perlin_gradient_noise/1d_perlin.ipynb
+
+
+def make_grid_coords(nsamples, start, end, dim, flatten=True):
+    if not isinstance(nsamples, Sequence):
+        nsamples = dim * [nsamples]
+    if not isinstance(start, Sequence):
+        start = dim * [start]
+    if not isinstance(end, Sequence):
+        end = dim * [end]
+    if len(nsamples) != dim or len(start) != dim or len(end) != dim:
+        raise ValueError("'nsamples'; 'start'; and 'end' should be a single value or have same  length as 'dim'")
+    
+    dir_samples = tuple([torch.linspace(start[i], end[i], steps=nsamples[i]) 
+                   for i in range(dim)])
+    grid = torch.stack(torch.meshgrid(*dir_samples, indexing='ij'), dim=-1)
+    return grid.reshape(-1, dim) if flatten else grid
+
+def rotation_matrix(axis, theta):
+    """
+    This function returns a 3x3 rotation matrix of an angle theta around one of the main axes in 3D.
+    
+    Args:
+    - axis: str, the axis to rotate around, 'x', 'y', or 'z'
+    - theta: float, the angle in radians to rotate by
+    
+    Returns:
+    - rot_mat: torch.Tensor of shape (3,3), the rotation matrix
+    """
+    assert axis in ['x', 'y', 'z'], "Axis must be 'x', 'y', or 'z'"
+    
+    if axis == 'x':
+        rot_mat = torch.tensor([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+    elif axis == 'y':
+        rot_mat = torch.tensor([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]])
+    else: # axis == 'z'
+        rot_mat = torch.tensor([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    
+    return rot_mat.float()
 
 def blend(x):
   return 6*x**5 - 15*x**4 + 10*x**3
@@ -34,7 +74,33 @@ def perlin_noise(samples, scale=10, octaves=1, p=1):
         pnoise = partial + pnoise
     return pnoise
 
+# only works for plane slices in 3D for now
+def make_domain_slices(nsamples, start, end, dircodes):
+    valid_codes = ['x', 'y', 'z', 'xy', 'xz', 'yz']
+    code_map = {
+        'x': [2, 0, 1],
+        'y': [0, 2, 1],
+        'z': [0, 1, 2]
+    }
+    
+    coords = make_grid_coords(nsamples, start, end, 2)
+    newdim = 0.0 * torch.ones((len(coords), 1))
+    domain_slice = torch.cat([coords, newdim], dim=-1)
+    slices = []
+    for code in dircodes:
+        if code not in valid_codes:
+            raise ValueError(
+                "Direction codes should be in [x, y, z, xy, xz, yz]")
+        domain_slice = domain_slice[:, code_map[code[0]]]
+        if len(code) == 2:
+            # todo, make angle a parameter
+            rotation = rotation_matrix(code[1], np.pi/4)
+            domain_slice = torch.matmul(rotation, 
+                                        domain_slice.permute((1, 0))
+                                        ).permute((1, 0))
+        slices.append(domain_slice.reshape(nsamples, nsamples, 3))
 
+    return slices
 
 # def checker(res, scale=10):
 #     tensors = tuple(3 * [torch.linspace(-1, 1, steps=res)])
@@ -76,22 +142,22 @@ if __name__ == '__main__':
     from PIL import Image
 
     # Define texture size and cube size
-    texture_size = 128
-    cube_size = 16
-
-    
+    # texture_size = 128
+    # cube_size = 16
 
     # Combine the sine waves and threshold to create checkerboard pattern
     # board = ((sine_x + sine_y + sine_z) > 0).astype(np.uint8) * 255
     # print(board.shape)
-    board = (solid_texture(texture_size, 0.4) * 255).astype(np.uint8)
-    Image.fromarray(board[0, :, :]).save('x.png')
-    Image.fromarray(board[:, 0, :]).save('y.png')
-    Image.fromarray(board[:, :, 0]).save('z.png')
+    # board = (solid_texture(texture_size, 0.4) * 255).astype(np.uint8)
+    # Image.fromarray(board[0, :, :]).save('x.png')
+    # Image.fromarray(board[:, 0, :]).save('y.png')
+    # Image.fromarray(board[:, :, 0]).save('z.png')
     # # Convert NumPy array to PIL image
     # image = Image.fromarray(checkerboard)
 
     # # Save the generated texture
     # image.save('checkerboard_sine_3d.png')
+    slices = make_domain_slices(32, -1, 1, ['x', 'y', 'z', 'xy'])
+    print(len(slices), slices[0].shape)
 
     