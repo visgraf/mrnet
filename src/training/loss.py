36d2d2810c9e41e30c27dadc4b55ec7bc6509653
Hallison Paz <hallpaz@impa.br>
2023-05-13 20:08:23 -0300

    cleaned after refactoring; trying L1 and L2 losses for hermite D1

--- src/training/loss.py
+++ src/training/loss.py
@@ -27,8 +27,7 @@ def hermite_loss(output_dict, train_dict, device):
 
     loss_dict['d0'] = F.mse_loss(pred, gt_pred) 
     # loss_dict['d1'] = F.l1_loss(pred_grad, gt_grad)
-    # loss_dict['d1'] = F.mse_loss(pred_grad, gt_grad)
-    loss_dict['d1'] = torch.mean((pred_grad - gt_grad).pow(2).sum(-1))
+    loss_dict['d1'] = F.mse_loss(pred_grad, gt_grad)
     return loss_dict
 
 def get_loss_from_map(lossname:str):
@@ -37,45 +36,3 @@ def get_loss_from_map(lossname:str):
         'hermite': hermite_loss
     }
     return valid_losses[lossname]
-
-# def old_mse_loss(batch, model, mrweights, device):
-#     loss_dict = {}
-
-#     pred_list = []
-#     gt_list = []
-
-#     output_dict, trainY = perform_inference(batch, model, mrweights, device, 'c0')
-
-#     train_dict = {k: v.to(device) for k, v in trainY.items()}
-#     pred_list.append(output_dict['model_out'])
-#     gt_list.append(train_dict['d0'])
-    
-#     pred = torch.cat(pred_list, dim=1)
-#     gt = torch.cat(gt_list, dim=1)
-
-#     loss_dict['d0'] = F.mse_loss(pred, gt) 
-
-#     return loss_dict
-
-# def old_hermite_loss(batch, model, mrweights,device):
-#     (trainX, trainY) = batch['c1']
-#     output_dict = model(trainX['coords'].to(device), mrweights=mrweights)
-#     train_dict = {k: v.to(device) for k, v in trainY.items()}
-#     coords = output_dict['model_in']
-    
-#     pred = output_dict['model_out']
-#     gt_grad = train_dict['d1']
-    
-#     pred_grad = gradient(pred, coords)
-#     pred_grad_flat = pred_grad.view(1,-1,1)
-
-#     loss_dict = mse_loss(batch, model, mrweights, device)
-#     loss_dict['d1'] = F.mse_loss(pred_grad_flat, gt_grad)/2
-
-#     return loss_dict
-
-# def perform_inference(batch, model, mrweights,device, class_points):
-#     (trainX, trainY) = batch[class_points]
-#     output_dict = model(trainX['coords'].to(device), mrweights=mrweights)
-
-#     return output_dict, trainY
