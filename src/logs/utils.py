84fcf150c7cd0bf1b601c69fe84fc27ab5819393
Hallison Paz <hallison@pgdinamica.com>
2023-05-15 01:49:08 -0300

    fixed bug on zoom visualization of colored images

--- src/logs/utils.py
+++ src/logs/utils.py
@@ -1,8 +1,17 @@
 import torch
 from datasets.sampler import make_grid_coords
 from torch.utils.data import BatchSampler
+from PIL import Image
 
-def output_per_batch(model, dataset, device):
+
+RESIZING_FILTERS = {
+    'nearest': Image.Resampling.NEAREST,
+    'linear': Image.Resampling.BILINEAR,
+    'cubic': Image.Resampling.BICUBIC,
+}
+
+
+def output_on_batched_dataset(model, dataset, device):
     model_out = []
     with torch.no_grad():
         for batch in dataset:
@@ -11,13 +20,11 @@ def output_per_batch(model, dataset, device):
             model_out.append(torch.clamp(output_dict['model_out'], 0.0, 1.0))
     return torch.concat(model_out)
 
-def output_on_batched_domain(model, nsamples, domain, dim, batch_size, device):
-    grid = make_grid_coords(nsamples, *domain, dim)
+def output_on_batched_grid(model, grid, batch_size, device):
     output = []
     for batch in BatchSampler(grid, batch_size, drop_last=False):
-        batch = torch.stack(batch)
-        with torch.no_grad():
-            output.append(model(batch.to(device))['model_out'])
+        batch = torch.stack(batch).to(device)
+        output.append(model(batch)['model_out'])
     return torch.concat(output)
 
 def ycbcr_to_rgb(image: torch.Tensor) -> torch.Tensor:
